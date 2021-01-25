51a4a84f90a6a8c192d37830ed97b954ded13ad2
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-01-25 12:09:17 -0800

    Add starter code

---
+++ starter-code/solution.py
@@ -0,0 +1,11 @@
+import ast
+import astor
+
+def myeval(s):
+    # step 1. string replacement
+    # step 2. parse
+    # step 3. node replacement
+    # step 4. dump / eval
+    return 'todo'
+
+print(myeval('1 + 2 * 3'))
